49b1e133de56c132bacae1a24fec6f1dda88f58b
Estuardo Ramírez <estuardito_9@hotmail.com>
2023-05-02 18:26:02 -0600

    Se creo la platilla base de ProyectoArduino.ino

---
+++ ProyectoArduino.ino
@@ -0,0 +1,11 @@
+/*
+Proyecto Universitario
+En este proyecto se han empleado librerías externas bajo la licencia de código abierto de ArduinoJSON y CBot, siendo propiedad de sus respectivos autores. Asimismo, se ha utilizado la librería ESP8266 para Arduino con el fin de operar ciertos módulos sin alterar el código original, y se ha atribuido el crédito correspondiente en la sección correspondiente a las bibliotecas utilizadas en el README.
+*/
+void setup() {
+
+}
+
+void loop() {
+
+}
